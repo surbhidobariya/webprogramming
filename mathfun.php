c69d989fb168cff68a15769feb1b585af6f911f0
surbhidobariya <154879923+surbhidobariya@users.noreply.github.com>
2024-01-11 08:57:39 +0530

    Add files via upload

---
+++ mathfun.php
@@ -0,0 +1,28 @@
+<?php
+$a=abs(-5);
+echo $a,'<br>';
+
+$b=sqrt(16);
+echo $b,'<br>';
+
+$c=rand(1,100);
+echo $c,'<br>';
+
+$d=round(3.7);
+echo $d,'<br>';
+
+$e=ceil(3.2);
+echo $e,'<br>';
+
+$f=floor(3.8);
+echo $f,'<br>';
+
+$g=pow(2,3);
+echo $g,'<br>';
+
+$h=max(5,8,3);
+echo $h,'<br>';
+
+$i=min(8,4,2);
+echo $i,'<br>';
+?>
