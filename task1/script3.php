cc07e7c7b68b9ca30835b72b163643483e0dbcc2
surbhidobariya <154879923+surbhidobariya@users.noreply.github.com>
2024-01-25 08:58:38 +0530

    Add files via upload

---
+++ task1/script3.php
@@ -0,0 +1,8 @@
+<?php
+
+$x=10;
+$y=20;
+$sum=$x+$y;
+echo "sum of x and y: $sum";
+
+?>
---
+++ task1/script9.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>person information</h1>";
+$nm="surbhi";
+$city="jetpur";
+$age=19;
+$rollno=26;
+echo "name is $nm<br> city is $city<br> age is $age<br> rollno $rollno";
+?>
